84d90c19f8654fc6443e2a36430aa01b20ab5d04
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-11-07 13:58:17 +0400

    Add starter codes for remaining tasks

---
+++ src/assignment/temperature_stats.py
@@ -0,0 +1,11 @@
+def get_tempreature_stats(file_path):
+    """
+    in given file each line will represent city name and temperature in celsius separated by comma
+
+    return a dictionary with following keys
+    - hottest_city: name of the city with highest temperature
+    - coldest_city: name of the city with lowest temperature
+    - average_temperature: average temperature of all cities
+    - temperature_range: difference between highest and lowest temperature
+    """
+    # wrtie your code here
---
+++ src/assignment/temperature_stats_v2.py
@@ -0,0 +1,12 @@
+def get_temperature_stats_v2(file_path):
+    """
+    in given file each line will represent city name and temperature in celsius separated by comma
+    each city can have multiple entries in the file
+
+    return a dictionary with following keys
+    - hottest_city: name of the city with highest temperature
+    - coldest_city: name of the city with lowest temperature
+    - average_temperature: average temperature of all cities
+    - temperature_range: difference between highest and lowest temperature
+    """
+    # wrtie your code here
