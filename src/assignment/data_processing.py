29b264a7e1a3c4f8f02f682e114c24ae82a6f019
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-11-07 13:21:33 +0400

    Add starter code for data processing

---
+++ src/assignment/data_processing.py
@@ -0,0 +1,8 @@
+def save_users(users, file_path):
+    """
+    function to save users to a file with folowing format:
+    "name,age\n"
+
+    do not return anything
+    """
+    # write your code here
