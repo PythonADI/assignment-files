45b8b40ad3666a915f879a5e95ef46f01ecd07bf
tavkhelidzeluka <tavkhelidzeluka@gmail.com>
2024-11-07 13:35:23 +0400

    Add starter code for random users

---
+++ src/assignment/random_users.py
@@ -0,0 +1,25 @@
+NAME_CHOICES = [
+    "John",
+    "Jane",
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Heidi",
+    "Ivan",
+    "Jack",
+]
+
+
+def generate_random_users(n, output_file):
+    """
+    Generate n random users and write them to a file.
+    Each user should be on a new line and have the format "Name,Age".
+    Name should be randomly selected from the NAME_CHOICES list.
+
+    Do not return anything, just write to the file.
+    """
+    # Write your code here
